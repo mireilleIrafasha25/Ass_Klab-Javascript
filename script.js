50450bed73f5dbbd3177d20c6b1fbeb5f4d69a80
mireilleIrafasha25 <mireilleirafasha@gmail.com>
2025-01-12 09:55:21 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// array containing object
+const projects = [
+    {
+      name: "SocialHub",
+      description: "A social media app for sharing posts.",
+      technologyUsed: ["Flutter", "Firebase"],
+      year: 2024
+    },
+    {
+      name: "E-Commerce Platform",
+      description: "An online shopping website.",
+      technologyUsed: ["React", "Node.js", "MongoDB"],
+      year: 2023
+    },
+    {
+      name: "Portfolio Website",
+      description: "A personal portfolio website.",
+      technologyUsed: ["HTML", "CSS", "JavaScript"],
+      year: 2022
+    }
+  ];
+  
+  // Using map to display project data
+  projects.map(project => {
+    console.log(`Project Name: ${project.name}`);
+    console.log(`Description: ${project.description}`);
+    console.log(`Technology Used: ${project.technologyUsed.join(", ")}`);
+    console.log(`Year: ${project.year}`);
+    console.log('---------------------------------------------------------------------------------------------------');
+  });
+  
